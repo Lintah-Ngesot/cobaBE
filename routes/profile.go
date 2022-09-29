6ba804539236e7fc332500cd3a948ceb6bf3a6de
Lintah-Ngesot <ujangsuryana588@gmail.com>
2022-09-29 21:44:45 +0700

    first commit

---
+++ routes/profile.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"literatur/handlers"
+	"literatur/pkg/mysql"
+	"literatur/repositories"
+
+	"github.com/gorilla/mux"
+)
+
+func ProfileRoutes(r *mux.Router) {
+	profileRepository := repositories.RepositoryProfile(mysql.DB)
+	h := handlers.HandlerProfile(profileRepository)
+
+	r.HandleFunc("/profiles/{id}", h.GetProfile).Methods("GET")
+}
